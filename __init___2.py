110528e01f13607b49e374f45044405bcd8ceff7
Luizmigsbr <160501822+Luizmigsbr@users.noreply.github.com>
2025-09-25 22:41:47 +0000

    Delete __init___2.py

--- __init___2.py
+++
@@ -1,7 +0,0 @@
-from .user import User
-from .study_session import StudySession
-from .question import Question
-from .progress import Progress
-
-__all__ = ['User', 'StudySession', 'Question', 'Progress']
-
