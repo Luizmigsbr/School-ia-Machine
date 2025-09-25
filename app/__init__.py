e15086f24c9d14650cb9e1a0c626927492299421
Luizmigsbr <160501822+Luizmigsbr@users.noreply.github.com>
2025-09-25 23:01:52 +0000

    Add files via upload

---
+++ app/__init__.py
@@ -0,0 +1,5 @@
+from .ai_integrations import AIServiceManager, get_ai_response, get_ai_service_status
+from .ml_algorithms import EducationalMLPipeline
+
+__all__ = ['AIServiceManager', 'get_ai_response', 'get_ai_service_status', 'EducationalMLPipeline']
+
