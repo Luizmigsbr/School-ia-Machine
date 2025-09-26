be041da9c9909cf2029b4b9d667b61bd9814c94d
Luizmigsbr <160501822+Luizmigsbr@users.noreply.github.com>
2025-09-26 01:00:04 +0000

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,13 @@
-  
+  # models/_init_.py
+
+# Importa a classe User do arquivo user.py e a torna disponível para o pacote 'models'
+from .user import User
+
+# Importa a classe StudySession do arquivo study_session.py
+from .study_session import StudySession
+
+# Importa a classe Question do arquivo question.py
+from .question import Question
+
+# Importa a classe Progress do arquivo progress.py
+from .progress import Progress
