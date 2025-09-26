7a70040728bcc0c549a295e3b58c5efa7351ac9d
Luizmigsbr <160501822+Luizmigsbr@users.noreply.github.com>
2025-09-26 00:19:50 +0000

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,1 @@
-from .ai_integrations import AIServiceManager, get_ai_response, get_ai_service_status
-from .ml_algorithms import EducationalMLPipeline
-
-__all__ = ['AIServiceManager', 'get_ai_response', 'get_ai_service_status', 'EducationalMLPipeline']
 
